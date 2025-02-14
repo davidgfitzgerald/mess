f4f9ec69a219cc9dfe1d41d995d9552c5fb6a534
David Fitzgerald <davidgfitzgerald@hotmail.co.uk>
2025-02-14 23:46:52 +0000

    Remove boilerplate

--- src/lib/Counter.svelte
+++ src/lib/Counter.svelte
@@ -8,3 +8,19 @@
 <button onclick={increment}>
   count is {count}
 </button>
+
+<style>
+  button {
+    border-radius: 8px;
+    border: 1px solid transparent;
+    padding: 0.6em 1.2em;
+    font-size: 1em;
+    font-weight: 500;
+    font-family: inherit;
+    cursor: pointer;
+    transition: border-color 0.1s;
+  }
+  button:hover {
+    border-color: #646cff;
+}
+</style>
